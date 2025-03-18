d7742bd66808e5d017ea3caa38bed3e2883bb434
joaomaggi03 <joaolucas333@gmail.com>
2025-03-18 20:29:52 -0300

    script menu nav

--- DOM.js
+++ DOM.js
@@ -1,15 +1,16 @@
-const linkServicos = document.getElementById("linkServicos")
-const linkSobre = document.getElementById("linkSobre")
-const linkInicio = document.getElementById("linkInicio")
+const linkServicos = document.getElementById("navServicos")
+const linkSobre = document.getElementById("navSobre")
+const linkInicio = document.getElementById("navInicio")
+const linkContato = document.getElementById("navContato")
 
 linkServicos.addEventListener("click", (e) => {
-    const elemento = document.getElementById("container4")
+    const elemento = document.getElementById("servicos1")
     e.preventDefault();
     window.scrollTo({top: elemento.offsetTop, behavior: "smooth"})
 })
 
 linkSobre.addEventListener("click", (e) => {
-    const elemento = document.getElementById("container3")
+    const elemento = document.getElementById("quem-somos")
     e.preventDefault();
     window.scrollTo({top: elemento.offsetTop, behavior: "smooth"})
 })
@@ -17,4 +18,4 @@ linkSobre.addEventListener("click", (e) => {
 linkInicio.addEventListener("click", (e) => {
     e.preventDefault();
     window.scrollTo({top: 0, behavior: "smooth"})
-})
+}) 
